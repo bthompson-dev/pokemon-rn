aee716aa54a298a61133f919c545f8d8f7f3d2ea
Ben Thompson <benthompson@MacBook-Pro.Metspace-F1>
2025-10-13 16:39:06 +0100

    added pokemon list to pokedex

--- components/Pokedex.tsx
+++ components/Pokedex.tsx
@@ -1,13 +1,27 @@
 import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { FlatList, StyleSheet } from 'react-native';
 
 import { HelloWave } from '@/components/hello-wave';
 import ParallaxScrollView from '@/components/parallax-scroll-view';
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
 
-export default function HomeScreen() {
+interface PokemonData {
+  id: number;
+  name: string;
+  url: string; 
+}
+
+function Pokemon( {pokemon} : {pokemon: PokemonData}) {
+  return (
+    <ThemedText style={styles.pokemon}>{pokemon.name}</ThemedText>
+  )
+}
+
+export default function Pokedex() {
+
+  const pokemonList = require("@/assets/pokemon.json");
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
@@ -21,58 +35,9 @@ export default function HomeScreen() {
         <ThemedText type="title">Pokedex Welcome</ThemedText>
         <HelloWave />
       </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
-              />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
-
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
+      <ThemedView style={styles.pokemonContainer}>
+        <FlatList data={pokemonList} renderItem={({item}) => <Pokemon pokemon={item} />} keyExtractor={item => item.id}
+        />
       </ThemedView>
     </ParallaxScrollView>
   );
@@ -84,9 +49,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     gap: 8,
   },
-  stepContainer: {
+  pokemonContainer: {
     gap: 8,
     marginBottom: 8,
+    backgroundColor: 'lightblue',
+  },
+  pokemon: {
+    marginVertical: 4,
+    marginHorizontal: 8,
+    fontSize: 18
   },
   reactLogo: {
     height: 178,
