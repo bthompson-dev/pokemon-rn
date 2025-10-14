d9de91cdbf6165e38ef91e0f944ae6de4413b6eb
Ben Thompson <benthompson@MacBook-Pro.Metspace-F1>
2025-10-14 14:21:33 +0100

    fix: switched out ParallaxScroll component to use FlatList directly, avoiding nested VirtualizedLists

--- components/Pokedex.tsx
+++ components/Pokedex.tsx
@@ -1,69 +1,81 @@
-import { Image } from 'expo-image';
-import { FlatList, StyleSheet } from 'react-native';
+import { Image } from "expo-image";
+import { FlatList, StyleSheet, View } from "react-native";
 
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
+import { HelloWave } from "@/components/hello-wave";
+import { ThemedText } from "@/components/themed-text";
+import { ThemedView } from "@/components/themed-view";
+
+const pokemonList = require("@/assets/pokemon.json");
 
 interface PokemonData {
   id: number;
   name: string;
-  url: string; 
+  url: string;
 }
 
-function Pokemon( {pokemon} : {pokemon: PokemonData}) {
-  return (
-    <ThemedText style={styles.pokemon}>{pokemon.name}</ThemedText>
-  )
+function Pokemon({ pokemon }: { pokemon: PokemonData }) {
+  return <ThemedText style={styles.pokemon}>{pokemon.name}</ThemedText>;
 }
 
 export default function Pokedex() {
-
-  const pokemonList = require("@/assets/pokemon.json");
-
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Pokedex Welcome</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.pokemonContainer}>
-        <FlatList data={pokemonList} renderItem={({item}) => <Pokemon pokemon={item} />} keyExtractor={item => item.id}
-        />
-      </ThemedView>
-    </ParallaxScrollView>
+    <FlatList
+      data={pokemonList}
+      renderItem={({ item }) => <Pokemon pokemon={item} />}
+      keyExtractor={(item) => item.id.toString()}
+      ListHeaderComponent={
+        <View style={styles.headerContainer}>
+          <ThemedView style={styles.titleContainer}>
+            <ThemedText type="title">Pokedex Welcome</ThemedText>
+            <HelloWave />
+          </ThemedView>
+          <Image
+            source={require("@/assets/images/partial-react-logo.png")}
+            style={styles.reactLogo}
+          />
+        </View>
+      }
+      ListHeaderComponentStyle={styles.header}
+      contentContainerStyle={styles.pokemonContainer}
+    />
   );
 }
 
 const styles = StyleSheet.create({
+  headerContainer: {
+    paddingTop: 32,
+    paddingBottom: 16,
+    backgroundColor: "white",
+    flex: 1,
+  },
   titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     gap: 8,
+    padding: 8,
+    marginTop: 100,
+    zIndex: 2,
   },
   pokemonContainer: {
     gap: 8,
     marginBottom: 8,
-    backgroundColor: 'lightblue',
+    backgroundColor: "lightblue",
   },
   pokemon: {
     marginVertical: 4,
     marginHorizontal: 8,
-    fontSize: 18
+    fontSize: 18,
   },
   reactLogo: {
     height: 178,
     width: 290,
     bottom: 0,
     left: 0,
-    position: 'absolute',
+    position: "absolute",
+  },
+  header: {
+    height: 200,
+    overflow: "hidden",
+    backgroundColor: "white",
   },
 });
